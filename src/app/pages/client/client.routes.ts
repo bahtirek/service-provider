1d619cc61c9190f9637acb6bc017ccc985d14f4d
bahtirek <bahtirek@gmail.com>
2023-12-27 00:19:59 -0500

    change: added client component and updated the routes

--- src/app/pages/client/client.routes.ts
+++ src/app/pages/client/client.routes.ts
@@ -1,14 +1,22 @@
 import { Route } from '@angular/router';
+import { ClientComponent } from './client.component';
 
 export const CLIENT_ROUTES: Route[] = [
-  { path: 'dashboard', loadComponent: () => import('./dashboard/dashboard.component').then(c => c.DashboardComponent) },
-  {
-    path: 'provider-list',
-    loadChildren: () => import('./provider-list/providers.routes').then((m) => m.PROVIDERS_ROUTES),
-  },
-  {
-    path: '',
-    redirectTo: 'dashboard',
-    pathMatch: 'full',
+  {path: '', component: ClientComponent,
+    children: [
+      {
+        path: 'dashboard',
+        loadComponent: () => import('./dashboard/dashboard.component').then(c => c.DashboardComponent)
+      },
+      {
+        path: 'provider-list',
+        loadChildren: () => import('./provider-list/providers.routes').then((m) => m.PROVIDERS_ROUTES),
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full',
+      },
+    ]
   },
 ];
