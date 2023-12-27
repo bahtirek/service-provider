272eb0bed975a5469cd595713f4ffee764727796
bahtirek <bahtirek@gmail.com>
2023-12-26 19:28:19 -0500

    change: created provider-card, subject-card components

---
+++ src/app/components/provider-card/provider-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-provider-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './provider-card.component.html',
+  styleUrl: './provider-card.component.scss'
+})
+export class ProviderCardComponent {
+
+}
---
+++ src/app/components/subject-card/subject-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subject-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './subject-card.component.html',
+  styleUrl: './subject-card.component.scss'
+})
+export class SubjectCardComponent {
+
+}
