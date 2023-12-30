1dcf7e2e42bf97db7ff45a7a50c495b882340fd3
bahtirek <bahtirek@gmail.com>
2023-12-30 08:18:42 -0500

    change: added error interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './shared/interceptors/auth.interceptor';
+import { errorInterceptor } from './shared/interceptors/error.interceptor';
 
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor]))
+    provideHttpClient(withInterceptors([authInterceptor, errorInterceptor]))
   ]
 };
--- src/app/shared/interceptors/error.interceptor.ts
+++ src/app/shared/interceptors/error.interceptor.ts
@@ -1,32 +1,15 @@
-// import { Injectable } from "@angular/core";
-// import {
-//   HttpEvent,
-//   HttpRequest,
-//   HttpHandler,
-//   HttpInterceptor,
-//   HttpErrorResponse
-// } from "@angular/common/http";
-// import { Observable, throwError } from "rxjs";
-// import { catchError, retry } from "rxjs/operators";
-// import { ToasterService } from "../shared/toaster/toaster.service";
-// import { paths } from "../data/paths";
+import { HttpInterceptorFn } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
 
-// @Injectable()
-// export class ErrorInterceptor implements HttpInterceptor {
-//   constructor(private toastr: ToasterService) {}
-//   intercept(
-//     req: HttpRequest<any>,
-//     next: HttpHandler
-//   ): Observable<HttpEvent<any>> {
-//     return next.handle(req).pipe(
-//       /* retry(2), */
-//       catchError((error: HttpErrorResponse) => {
-//         if (error.status !== 401) {
-//           // 401 handled in auth.interceptor
-//           this.toastr.show('error', 'Sorry!', 'Something went wrong. Please try later. ')
-//         }
-//         return throwError(error);
-//       })
-//     );
-//   }
-// }
+export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+  return next(req).pipe(
+    catchError((error) => {
+      if([401, 403].includes(error.status)) {
+        console.log('error');
+        alert('You are not authorized to access this page.');
+
+      }
+      return throwError(() => error);
+    })
+  )
+};
