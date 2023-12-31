ff4b369a2479b1f0cdf6baa347548fa75ad63431
bahtirek <bahtirek@gmail.com>
2023-12-31 15:28:46 -0500

    change: removed unused code

--- src/app/components/toaster/toaster.service.ts
+++ src/app/components/toaster/toaster.service.ts
@@ -8,15 +8,6 @@ import { Toast, ToastType } from '../../shared/interfaces/toaster.interface';
 })
 export class ToasterService {
 
-  subject: BehaviorSubject<Toast>;
-  toast$: Observable<Toast>;
-
-  constructor() {
-    this.subject = new BehaviorSubject<Toast>({} as Toast);
-    this.toast$ = this.subject.asObservable()
-      .pipe(filter(toast => toast !== null));
-  }
-
   toasts = signal<Toast[]>([]);
 
   show(type: ToastType, title?: string, body?: string, delay?: number) {
@@ -28,6 +19,7 @@ export class ToasterService {
   removeToast(index: number) {
     this.toasts().splice(index, 1);
   }
+
   removeLastToast() {
     this.toasts().pop();
   }
