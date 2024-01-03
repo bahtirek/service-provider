affc13a1e36ea8a7aaa18faf21c140c51b692389
bahtirek <bahtirek@gmail.com>
2024-01-02 23:49:56 -0500

    change: created category check component and added to provider saerch

--- src/app/components/provider-search/provider-search.component.ts
+++ src/app/components/provider-search/provider-search.component.ts
@@ -3,11 +3,13 @@ import { ProviderService } from '../../shared/services/provider.service';
 import { FormErrorComponent } from '../../shared/form-helpers/form-error/form-error.component';
 import { FormsModule } from '@angular/forms';
 import { Provider } from '../../shared/interfaces/provider.interface';
+import { CategorySelectComponent } from '../category-select/category-select.component';
+import { CategoryCheckComponent } from '../category-check/category-check.component';
 
 @Component({
   selector: 'app-provider-search',
   standalone: true,
-  imports: [FormErrorComponent, FormsModule],
+  imports: [FormErrorComponent, FormsModule, CategorySelectComponent, CategoryCheckComponent],
   templateUrl: './provider-search.component.html',
   styleUrl: './provider-search.component.scss'
 })
@@ -37,6 +39,16 @@ export class ProviderSearchComponent {
         console.log(err);
       }
     })
+  }
+
+  onCategoryCheck(categorys: number[]){
+    console.log(categorys);
+
+  }
+
+  onCategorySelect(categoryId: number) {
+    console.log(categoryId);
+
   }
 }
 
