8ed40e0ae00618a24820bfb5af6c29915fd66c44
bahtirek <bahtirek@gmail.com>
2024-01-07 00:09:23 -0500

    change: error interceptor message fix

--- src/app/shared/interceptors/error.interceptor.ts
+++ src/app/shared/interceptors/error.interceptor.ts
@@ -7,14 +7,25 @@ import { AuthService } from '../services/auth.service';
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
   const toaster = inject(ToasterService);
   const authService = inject(AuthService);
-  const getRandomInt = (max: number) => {
-    return Math.floor(Math.random() * max);
-  }
+
   return next(req).pipe(
     catchError((error) => {
       if([401, 403].includes(error.status)) {
-        toaster.show('error', `Sorry!`, 'Unathorized. Please login again.12');
-        //authService.logout();
+        toaster.show('error', `Sorry!`, 'Unathorized. Please login again.');
+        authService.refreshToken();
+      } else if(error.status == 400){
+
+        let errorMessage = "";
+        if(error.error.message) {
+          error.error.message.forEach((message: string) => {
+            errorMessage = errorMessage + message + '. \n'
+          })
+        } else {
+          errorMessage = "Something went wrong. Please try later.";
+        }
+        toaster.show('error', 'Sorry!', errorMessage)
+      } else if(error.status == 500){
+        toaster.show('error', 'Sorry!', error.error.message)
       } else {
         toaster.show('error', 'Sorry!', 'Something went wrong. Please try later. ')
       }
