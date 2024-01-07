551493dbc55b6deb8bd75f9d7c64c458e45d2123
bahtirek <bahtirek@gmail.com>
2024-01-06 22:50:59 -0500

    change: uncommented code

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -90,7 +90,7 @@ export class AuthService {
         },
         error: (err) => {
           this.isLoggedIn.set(false);
-          //this.logout('login');
+          this.logout('login');
         }
       })
     }
