dffc85181e07683a43b744c6eb94b0489b160565
bahtirek <bahtirek@gmail.com>
2024-02-04 11:48:48 -0500

    change: get video thumbnail

--- src/app/pages/messages/file-upload/file-details/file-details.component.ts
+++ src/app/pages/messages/file-upload/file-details/file-details.component.ts
@@ -13,11 +13,12 @@ export class FileDetailsComponent {
   file?: File;
   fileUrl: string | ArrayBuffer | null = '';
   fileType: string = 'UNK'
+  thumbnail?: string;
 
   @Input() set fileProp(file: File){
-    this.fileType = this.setFileType(file.type);
-    console.log(this.fileType);
     this.file = file;
+    this.setFileType(file.type);
+    console.log(this.file);
     if(this.fileType != 'IMG') return;
     const reader = new FileReader();
     reader.onload = (e) => {
@@ -27,17 +28,52 @@ export class FileDetailsComponent {
     reader.readAsDataURL(file);
   }
 
-  setFileType(type: string) {
+  async setFileType(type: string) {
+    console.log(this.file);
+    console.log(type.includes('video') || type.includes('avi'));
+    console.log(type);
+
+
     if(type.includes('image')) {
-      return 'IMG'
+      this.fileType =  'IMG'
     } else if (type.includes('pdf')) {
-      return 'PDF'
+      this.fileType =  'PDF'
     } else if (type.includes('zip')) {
-      return 'ZIP'
+      this.fileType =  'ZIP'
     } else if (type.includes('document')) {
-      return 'DOC'
+      this.fileType =  'DOC'
+    } else if (type.includes('video')) {
+      if(type.includes('mp4') || type.includes('ogg') || type.includes('mov') || type.includes('webm')) {
+        const thumbnail =  await this.generateVideoThumbnail(this.file!);
+        this.fileUrl = `url("${thumbnail}")`;
+      }
+      this.fileType =  'VID'
     } else {
-      return 'UNK'
+      this.fileType =  'UNK'
     }
   }
+
+
+  generateVideoThumbnail(file: File):Promise<string> {
+    return new Promise((resolve) => {
+      const canvas = document.createElement("canvas");
+      const video = document.createElement("video");
+
+      // this is important
+      video.autoplay = true;
+      video.muted = true;
+      video.src = URL.createObjectURL(file);
+
+      video.onloadeddata = () => {
+        let ctx = canvas.getContext("2d");
+
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+        if(ctx == null) return null;
+        ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+        video.pause();
+        return resolve(canvas.toDataURL("image/png"));
+      };
+    });
+  };
 }
