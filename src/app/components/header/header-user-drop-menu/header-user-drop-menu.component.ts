0f25231ef489f62ad29c698d6c95b862fdbffd96
bahtirek <bahtirek@gmail.com>
2024-02-05 00:14:30 -0500

    change: header drop menu mobile view

--- src/app/components/header/header-user-drop-menu/header-user-drop-menu.component.ts
+++ src/app/components/header/header-user-drop-menu/header-user-drop-menu.component.ts
@@ -1,12 +1,12 @@
-import { Component, computed, inject } from '@angular/core';
-import { HeaderNavMenu } from '../../../shared/interfaces/header-nav-menu.interface';
+import { Component, EventEmitter, Input, Output, computed, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../../../shared/services/auth.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-header-user-drop-menu',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgClass],
   templateUrl: './header-user-drop-menu.component.html',
   styleUrl: './header-user-drop-menu.component.scss'
 })
@@ -14,23 +14,37 @@ export class HeaderUserDropMenuComponent {
 
   private auth = inject(AuthService);
 
-  loggedIn = computed(() =>
-    this.auth.isLoggedIn()
-  );
+  modalValue: boolean = false;
+  fadeAnim: boolean = false;
+  fadeIn: boolean = false;
 
-  logout = () => {
-    this.auth.logout('home');
+  @Output() modalChange = new EventEmitter<boolean>();
+  hideContainer: boolean = true;
+
+  @Input() get modal(){
+    return this.modalValue;
   }
 
-  userSubMenuItems: HeaderNavMenu[] = [
-    {
-      name: 'My providers',
-      url: 'client/dashboard',
-    },
-    {
-      name: 'Account',
-      url: '/account'
-    },
+  set modal(val) {
+    if (val) {
+      this.hideContainer = false;
+      this.fadeAnim = true;
+      setTimeout(() => {
+        this.fadeIn = true;
+      });
+    } else {
+      this.fadeIn = false;
+      setTimeout(() => {
+        this.fadeAnim = false;
+        this.hideContainer = true;
+      }, 400);
+    }
 
-  ]
+    this.modalValue = val;
+    this.modalChange.emit(this.modalValue);
+  }
+
+  closeModal(){
+    this.modal = false;
+  }
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, computed, inject } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { HeaderNavMenuComponent } from './header-nav-menu/header-nav-menu.component';
 import { HeaderUserMenuComponent } from './header-user-menu/header-user-menu.component';
+import { HeaderUserDropMenuComponent } from './header-user-drop-menu/header-user-drop-menu.component';
+import { AuthService } from '../../shared/services/auth.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [HeaderNavMenuComponent, HeaderUserMenuComponent, RouterLink],
+  imports: [HeaderNavMenuComponent, HeaderUserMenuComponent, RouterLink, HeaderUserDropMenuComponent, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  private auth = inject(AuthService);
+  toggleMenuModal: boolean = false;
 
+  openMenu(){
+    this.toggleMenuModal = true
+  }
+
+  closeMenu(){
+    this.toggleMenuModal = false
+  }
+
+  loggedIn = computed(() =>
+    this.auth.isLoggedIn()
+  );
 }
