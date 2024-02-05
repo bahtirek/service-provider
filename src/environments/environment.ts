30518fc4ff354b22992ae2f4bf7c83c793b1f4fa
bahtirek <bahtirek@gmail.com>
2024-02-05 00:13:51 -0500

    change: updated environment url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://73.57.80.184:3000/api',
-  socketUrl: 'http://73.57.80.184:3000',
+  apiUrl: 'https://nodejs-production-e941.up.railway.app/api',
+  socketUrl: 'https://nodejs-production-e941.up.railway.app',
 };
