46656b44d79d740fe2ae425a849dc5e9d8ea3782
bahtirek <bahtirek@gmail.com>
2024-02-14 20:37:57 -0500

    change: messenger header

--- src/app/components/back-button/back-button.component.ts
+++ src/app/components/back-button/back-button.component.ts
@@ -1,15 +1,18 @@
-import { Location } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Location, NgClass } from '@angular/common';
+import { Component, Input, inject } from '@angular/core';
 
 @Component({
   selector: 'app-back-button',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './back-button.component.html',
   styleUrl: './back-button.component.scss'
 })
 export class BackButtonComponent {
   private location = inject(Location);
+
+  @Input() class: string = 'my-md';
+  @Input() color: string = '';
 
   goBack(): void {
     this.location.back();
