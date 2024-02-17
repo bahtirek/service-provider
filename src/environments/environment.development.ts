76ef858128d7f57ec08ffc750245b5fde5f2f73b
bahtirek <bahtirek@gmail.com>
2024-02-16 23:12:48 -0500

    change: message font size

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  /* apiUrl: 'http://73.57.80.184:3000/api',
-  socketUrl: 'http://73.57.80.184:3000', */
-  apiUrl: 'https://nodejs-production-e941.up.railway.app/api',
-  socketUrl: 'https://nodejs-production-e941.up.railway.app',
+  apiUrl: 'http://73.57.80.184:3000/api',
+  socketUrl: 'http://73.57.80.184:3000',
+  /* apiUrl: 'https://nodejs-production-e941.up.railway.app/api',
+  socketUrl: 'https://nodejs-production-e941.up.railway.app', */
 };
