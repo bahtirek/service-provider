f90f7984944400018c86f17218144ce3c7309260
bahtirek <bahtirek@gmail.com>
2024-02-18 01:08:16 -0500

    change: provder list instead of cards

--- src/app/components/provider/provider-list/provider-list.component.ts
+++ src/app/components/provider/provider-list/provider-list.component.ts
@@ -1,17 +1,21 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Provider } from '../../../shared/interfaces/provider.interface';
 import { ProviderCardComponent } from '../provider-card/provider-card.component';
+import { ProviderListItemComponent } from '../provider-list-item/provider-list-item.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-provider-list',
   standalone: true,
-  imports: [ProviderCardComponent],
+  imports: [ProviderCardComponent, ProviderListItemComponent, NgClass],
   templateUrl: './provider-list.component.html',
   styleUrl: './provider-list.component.scss'
 })
 export class ProviderListComponent {
   providerList: Provider[] = [];
 
+
+  @Input() listTypeCard: boolean = false;
   @Input() set providers (providers: Provider[]) {
     this.providerList = providers
   }
