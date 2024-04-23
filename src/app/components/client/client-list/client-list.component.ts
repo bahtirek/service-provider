3da6e3ad10a3a2121f97af5eb8a96e0bfb7b7b59
bahtirek <bahtirek@gmail.com>
2024-04-22 23:55:43 -0400

    change: updated client list for client item component

--- src/app/components/client/client-list/client-list.component.ts
+++ src/app/components/client/client-list/client-list.component.ts
@@ -1,20 +1,27 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Client } from '../../../shared/interfaces/client.interface';
 import { ClientCardComponent } from '../client-card/client-card.component';
+import { ClientItemComponent } from '../client-item/client-item.component';
 
 @Component({
   selector: 'app-client-list',
   standalone: true,
-  imports: [ClientCardComponent],
+  imports: [ClientCardComponent, ClientItemComponent],
   templateUrl: './client-list.component.html',
   styleUrl: './client-list.component.scss'
 })
 export class ClientListComponent {
   clientList: Client[] = [];
+  isCard: boolean = true;
 
   @Input() set clients (clients: Client[]) {
     this.clientList = clients
     console.log(clients);
+  }
+
+  @Input() set type (type: string) {
+    if (type == 'item') this.isCard = false;
+    console.log(type);
 
   }
 
